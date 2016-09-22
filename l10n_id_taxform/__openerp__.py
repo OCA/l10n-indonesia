3941651cf388e3a8e3f57a66305480040c3660df
Andhitia Rama <andhitia.r@gmail.com>
2016-09-22 16:19:11 +0700

    [ADD][8.0] l10n_id_taxform

---
+++ l10n_id_taxform/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia's Taxform",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/taxform_groups.xml",
+        "menu.xml",
+    ],
+}
