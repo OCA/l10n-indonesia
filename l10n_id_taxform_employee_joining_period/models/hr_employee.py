21f060cd890b1cfa96259d7f99ea0fa4b9afdaed
Andhitia Rama <andhitia.r@gmail.com>
2016-09-30 19:10:05 +0700

    Add quit period field

--- l10n_id_taxform_employee_joining_period/models/hr_employee.py
+++ l10n_id_taxform_employee_joining_period/models/hr_employee.py
@@ -10,10 +10,15 @@ class HrEmployee(models.Model):
 
     @api.depends(
         "contract_ids",
-        "contract_ids.date_start")
+        "contract_ids.date_start",
+        "contract_ids.date_end")
     @api.multi
     def _compute_tax_period(self):
         for employee in self:
+            employee.joining_tax_period_id = False
+            employee.joining_tax_year_id = False
+            employee.quiting_tax_period_id = False
+            employee.quiting_tax_year_id = False
             obj_contract = self.env["hr.contract"]
             criteria = [
                 ("employee_id", "=", employee.id),
@@ -21,8 +26,6 @@ class HrEmployee(models.Model):
             contract = obj_contract.search(
                 criteria, order="date_start asc", limit=1)
             if not contract:
-                employee.joining_tax_period_id = False
-                employee.joining_tax_year_id = False
                 continue
 
             try:
@@ -32,18 +35,46 @@ class HrEmployee(models.Model):
                 employee.joining_tax_period_id = period
                 employee.joining_tax_year_id = period.year_id
             except:
-                employee.joining_tax_period_id = False
-                employee.joining_tax_year_id = False
+                continue
+            criteria = [
+                ("employee_id", "=", employee.id),
+                ]
+            contract = obj_contract.search(
+                criteria, order="date_start desc", limit=1)
+            if not contract:
+                continue
+            if not contract.date_end:
+                continue
+            try:
+                obj_period = self.env["l10n_id.tax_period"]
+                period = obj_period._find_period(
+                    contract.date_end)
+                employee.quiting_tax_period_id = period
+                employee.quiting_tax_year_id = period.year_id
+            except:
+                continue
 
     joining_tax_period_id = fields.Many2one(
         string="Joining Tax Period",
         comodel_name="l10n_id.tax_period",
         compute="_compute_tax_period",
-        store=True,
+        store=False,
         )
     joining_tax_year_id = fields.Many2one(
         string="Joining Tax Year",
         comodel_name="l10n_id.tax_year",
         compute="_compute_tax_period",
-        store=True,
+        store=False,
+        )
+    quiting_tax_period_id = fields.Many2one(
+        string="Quiting Tax Period",
+        comodel_name="l10n_id.tax_period",
+        compute="_compute_tax_period",
+        store=False,
+        )
+    quiting_tax_year_id = fields.Many2one(
+        string="Quiting Tax Year",
+        comodel_name="l10n_id.tax_year",
+        compute="_compute_tax_period",
+        store=False,
         )
