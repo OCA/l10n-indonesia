0e573e94ceb52283fa6d0c1fa5097ba0e5f01aac
Andhitia Rama <andhitia.r@gmail.com>
2017-01-16 17:39:24 +0700

    [IMP] l10n_id_taxform_period: black, isort, prettier

--- l10n_id_taxform_period/__init__.py
+++ l10n_id_taxform_period/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-# -*- coding: utf-8 -*-
->>>>>>> 1422ada ([IMP] l10n_id_taxform: black, isort, prettier)
 # Copyright 2016 Prime Force Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
