16314dcaf110704fdb62c61784357675da16c697
mike <viriyananda.michael@gmail.com>
2017-02-21 10:29:00 +0700

    Create Unittest

--- l10n_id_taxform_pph_21_payslip/tests/__init__.py
+++ l10n_id_taxform_pph_21_payslip/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# from . import test_hr_employee
+from . import test_compute_payslip_tax_period
