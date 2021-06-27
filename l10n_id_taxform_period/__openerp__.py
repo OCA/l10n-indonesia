639e50c34d55462ddb5efe2c7951756a3599f581
Nurazmi <azmimr67@gmail.com>
2021-06-27 14:02:01 +0700

    [8.0.1.1.0] l10n_id_taxform_pph_21_payslip Task T003241 : travis dll Menambah salary rule untuk tunjangan PPH

--- l10n_id_taxform_period/__openerp__.py
+++ l10n_id_taxform_period/__openerp__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
+# Copyright 2021 PT. Simetri Sinergi Indonesia
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia's Taxform - Tax Period",
     "version": "8.0.1.0.0",
     "category": "Hidden",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia,"
-              "Odoo Community Association (OCA)",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia,"
+    " Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
