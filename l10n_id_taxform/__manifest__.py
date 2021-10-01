9b6f4ffe04829927029d89b3b4cc2185026a4ec6
Panca <ppakpahan@solusiaglis.co.id>
2021-10-01 12:07:20 +0700

    [MIG] l10n_id_taxform: Migration to 12.0

--- l10n_id_taxform/__manifest__.py
+++ l10n_id_taxform/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia's Taxform",
-    "version": "8.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia," "Odoo Community Association (OCA)",
