55afb7cbf2f1e1523ad12bea3b7da6ab888cdcd4
Panca <ppakpahan@solusiaglis.co.id>
2021-12-22 21:24:55 +0700

    [IMP] l10n_id_taxform: black, isort, prettier

--- l10n_id_taxform_period/tests/test_tax_period.py
+++ l10n_id_taxform_period/tests/test_tax_period.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -9,6 +10,19 @@ from odoo.tests.common import TransactionCase
 
 
 class TaxPeriodCase(TransactionCase):
+=======
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests.common import TransactionCase
+from datetime import datetime
+from dateutil import relativedelta
+
+
+class TaxPeriodCase(TransactionCase):
+
+>>>>>>> 1422ada ([IMP] l10n_id_taxform: black, isort, prettier)
     def setUp(self, *args, **kwargs):
         super(TaxPeriodCase, self).setUp(*args, **kwargs)
 
@@ -17,13 +31,19 @@ class TaxPeriodCase(TransactionCase):
         self.demo_year = self.env.ref("l10n_id_taxform_period.data_tax_year")
 
     def test_create_year(self):
+<<<<<<< HEAD
         next_year = (
             datetime.strptime(str(self.demo_year.date_start), "%Y-%m-%d").year + 1
         )
+=======
+        next_year = datetime.strptime(
+            self.demo_year.date_start, "%Y-%m-%d").year + 1
+>>>>>>> 1422ada ([IMP] l10n_id_taxform: black, isort, prettier)
         date_start_year = datetime(next_year, 1, 1)
         date_end_year = datetime(next_year, 12, 31)
         date_end_period = datetime(next_year, 1, 31)
         # User create Tax Year
+<<<<<<< HEAD
         tax_year = self.obj_year.create(
             {
                 "name": str(next_year),
@@ -32,11 +52,22 @@ class TaxPeriodCase(TransactionCase):
                 "date_end": date_end_year.strftime("%Y-%m-%d"),
             }
         )
+=======
+        tax_year = self.obj_year.create({
+            "name": str(next_year),
+            "code": str(next_year),
+            "date_start": date_start_year.strftime(
+                "%Y-%m-%d"),
+            "date_end": date_end_year.strftime(
+                "%Y-%m-%d"),
+        })
+>>>>>>> 1422ada ([IMP] l10n_id_taxform: black, isort, prettier)
         # User create tax period
         tax_year.action_create_period()
         # Assert date_start and date_end periods that created
         for month in range(0, 12):
             self.assertEqual(
+<<<<<<< HEAD
                 tax_year.period_ids[month].date_start.strftime("%Y-%m-%d"),
                 (date_start_year + relativedelta.relativedelta(months=+month)).strftime(
                     "%Y-%m-%d"
@@ -63,3 +94,32 @@ class TaxPeriodCase(TransactionCase):
         self.assertEqual(
             tax_year.period_ids[2]._previous_period(1), tax_year.period_ids[1]
         )
+=======
+                tax_year.period_ids[month].date_start,
+                (date_start_year +
+                 relativedelta.relativedelta(
+                     months=+month)).strftime("%Y-%m-%d"))
+            self.assertEqual(
+                tax_year.period_ids[month].date_end,
+                (date_end_period +
+                 relativedelta.relativedelta(
+                     months=+month)).strftime("%Y-%m-%d"))
+        # Check l10n_id.tax_year _find method
+        self.assertEqual(
+            self.obj_year._find_year(
+                date_start_year.strftime("%Y-%m-%d")),
+            tax_year)
+        # Check l1on_id.tax_period _find method
+        self.assertEqual(
+            self.obj_period._find_period(
+                date_start_year.strftime("%Y-%m-%d")),
+            tax_year.period_ids[0])
+        # Check forward find l1on_id.tax_period _find method
+        self.assertEqual(
+            tax_year.period_ids[1]._next_period(1),
+            tax_year.period_ids[2])
+        # Check backward find l1on_id.tax_period _find method
+        self.assertEqual(
+            tax_year.period_ids[2]._previous_period(1),
+            tax_year.period_ids[1])
+>>>>>>> 1422ada ([IMP] l10n_id_taxform: black, isort, prettier)
