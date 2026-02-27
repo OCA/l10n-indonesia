68041f8170c75e8712ad2ac491f73770c4f5a64a
hitrosol <ppakpahan@gmail.com>
2026-02-27 21:45:48 +0700

    [16.0] [ADD] currency_rate_update_bi: Add Bank Indonesia currency rate provider
    Add new module to fetch exchange rates from Bank Indonesia (BI) webservice.
    
    Features:
    - Fetches rates from BI Kurs Transaksi webservice
    - Supports 19 currencies (USD, EUR, GBP, JPY, etc.)
    - Works with both IDR base and foreign base currencies
    - Handles cross-rate calculations for foreign base
    - Full test coverage with mock responses

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-l10n-indonesia",
+    description="Meta package for oca-l10n-indonesia Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-currency_rate_update_BI>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
